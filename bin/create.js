e4d8101cf79ee121613d70e2548a984a8f9480d3
Josh Ferge <josh.ferge@gmail.com>
2024-09-12 19:50:09 -0700

    add create app command

---
+++ bin/create.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const { execa } = require("execa");
+const path = require("path");
+
+const repoUrl = "https://github.com/JoshFerge/scaffold.git";
+const projectName = process.argv[2] || "my-new-project";
+
+async function main() {
+  console.log(`Creating new project: ${projectName}`);
+
+  try {
+    // Clone the repository
+    await execa("git", ["clone", repoUrl, projectName]);
+
+    // Remove the .git folder to disassociate from the original repo
+    await execa("rm", ["-rf", path.join(projectName, ".git")]);
+
+    console.log(`Project created successfully in ./${projectName}`);
+    console.log("To get started, run:");
+    console.log(`  cd ${projectName}`);
+    console.log("  npm install");
+  } catch (error) {
+    console.error("An error occurred:", error.message);
+  }
+}
+
+main();
