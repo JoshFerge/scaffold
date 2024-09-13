d1d7a21e41d3048eebc6f7bae2dbf3a19603b18c
Josh Ferge <josh.ferge@gmail.com>
2024-09-12 20:28:15 -0700

    replace next auth

--- bin/create.js
+++ bin/create.js
@@ -1,20 +1,42 @@
 #!/usr/bin/env node
 
-import pkg from "execa";
-const { execa } = pkg;
+import { exec } from "child_process";
+import { promisify } from "util";
 import path from "path";
+import fs from "fs/promises";
 
-const repoUrl = "https://github.com/JoshFerge/scaffold.git";
+const execAsync = promisify(exec);
+
+const repoUrl = "git@github.com:JoshFerge/scaffold.git";
 const projectName = process.argv[2] || "my-new-project";
+
 async function main() {
   console.log(`Creating new project: ${projectName}`);
 
   try {
     // Clone the repository
-    await execa("git", ["clone", repoUrl, projectName]);
+    await execAsync(`git clone ${repoUrl} ${projectName}`);
 
     // Remove the .git folder to disassociate from the original repo
-    await execa("rm", ["-rf", path.join(projectName, ".git")]);
+    await execAsync(`rm -rf ${path.join(projectName, ".git")}`);
+
+    // Replace "scaffold" with projectName in package.json
+    const packageJsonPath = path.join(projectName, "package.json");
+    let packageJson = await fs.readFile(packageJsonPath, "utf-8");
+    packageJson = packageJson.replace(/scaffold/g, projectName);
+    await fs.writeFile(packageJsonPath, packageJson);
+
+    // Generate a random AUTH_SECRET and update wrangler.toml
+    const wranglerTomlPath = path.join(projectName, "wrangler.toml");
+    let wranglerToml = await fs.readFile(wranglerTomlPath, "utf-8");
+    const authSecret = (
+      await execAsync("openssl rand -base64 32")
+    ).stdout.trim();
+    wranglerToml = wranglerToml.replace(
+      /AUTH_SECRET="<REPLACE_ME>"/,
+      `AUTH_SECRET="${authSecret}"`
+    );
+    await fs.writeFile(wranglerTomlPath, wranglerToml);
 
     console.log(`Project created successfully in ./${projectName}`);
     console.log("To get started, run:");
